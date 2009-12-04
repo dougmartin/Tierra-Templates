0e864e1f43ab3d05eee66a9efb0165f738cd54a0
Doug Martin <doug@tierra-innovation.com>
2009-12-04 11:04:21 -0500

    [FIXED] bug in append - appended blocks should emit if they are within other blocks

--- src/TierraTemplateCodeGenerator.php
+++ src/TierraTemplateCodeGenerator.php
@@ -303,7 +303,7 @@
 					case "append":
 					case "set":
 						$code[] = "\$this->__request->{$openingBlock->command}Block('{$node->blockName}', ob_get_contents()); ob_end_clean();";
-						if (!$this->isChildTemplate)
+						if ((count($this->blockStack) > 0) || !$this->isChildTemplate)
 							$code[] = "\$this->__request->echoBlock('{$node->blockName}');";
 						break;
 				}
