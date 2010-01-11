b7c8f9fa16e3843a6613035c6731e81f31a64ccb
Doug Martin <doug@tierra-innovation.com>
2010-01-11 11:13:09 -0500

    [ADDED] public and private build configs

---
+++ src/runner/private_config.php
@@ -0,0 +1,7 @@
+<?php
+
+	return array(
+		"srcDir" => dirname(__FILE__) . "/../private/lib/tierratemplates",
+		"cacheDir" => dirname(__FILE__) . "/../private/cache",
+		"baseTemplateDir" => dirname(__FILE__) . "/../private/templates",
+	);
